4b0e8f6782cd1c836cb1319e305911ca79546216
Clément Nerma <clement.nerma@gmail.com>
2020-08-20 09:55:47 +0200

    Add a background image

---
+++ source/js/fluid_custom.js
@@ -0,0 +1,4 @@
+$('footer > div > div').append(
+    '<i class="iconfont icon-love"></i> ' +
+    '<a href="https://www.deviantart.com/rmradev" style="color: blue;">rmRadev</a> (background image)'
+)
