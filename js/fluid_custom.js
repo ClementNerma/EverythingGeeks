74f444148c5aabd437071d571d41641f1d315ea2
ClementNerma <ClementNerma@users.noreply.github.com>
2024-06-01 17:54:34 +0000

    deploy: 8f35863b8c67f666b98ea23491a1837d1569d150

--- js/fluid_custom.js
+++
@@ -1,4 +0,0 @@
-$('footer > div > div').append(
-    '<i class="iconfont icon-love"></i> ' +
-    '<a href="https://www.deviantart.com/rmradev" style="color: blue;">rmRadev</a> (background image)'
-)
